c85df2fbb2fe7869a292455f9bcf1994c38d367c
Adrian Lafond <mail@adrianlafond.com>
2023-12-01 16:12:21 -0500

    test cards loading

--- src/components/card-loader/card-loader.tsx
+++ src/components/card-loader/card-loader.tsx
@@ -4,13 +4,15 @@ import { filenames, imgAlts } from '../../cards'
 import './styles.css'
 
 export interface CardLoaderProps {
+  imgSrcPaths?: string[],
   onComplete?: () => void
   onError?: () => void
 }
 
-export function CardLoader ({
+export function CardLoader({
+  imgSrcPaths,
   onComplete,
-  onError
+  onError,
 }: CardLoaderProps): JSX.Element {
   const complete = useRef(0)
 
@@ -20,12 +22,14 @@ export function CardLoader ({
     }
   }
 
+  const paths = imgSrcPaths ?? filenames;
+
   return (
-    <div class='card-loader'>
-      <div class='card-loader__suit'>
-        {filenames.slice(0, 13).map((filename, index) => (
+    <div class="card-loader">
+      <div class="card-loader__suit">
+        {paths.slice(0, 13).map((filename, index) => (
           <img
-            class='card-loader__img'
+            class="card-loader__img"
             key={filename}
             onAbort={onError}
             onError={onError}
@@ -35,10 +39,10 @@ export function CardLoader ({
           />
         ))}
       </div>
-      <div class='card-loader__suit'>
-        {filenames.slice(13, 26).map((filename, index) => (
+      <div class="card-loader__suit">
+        {paths.slice(13, 26).map((filename, index) => (
           <img
-            class='card-loader__img'
+            class="card-loader__img"
             key={filename}
             onAbort={onError}
             onError={onError}
@@ -48,10 +52,10 @@ export function CardLoader ({
           />
         ))}
       </div>
-      <div class='card-loader__suit'>
-        {filenames.slice(26, 39).map((filename, index) => (
+      <div class="card-loader__suit">
+        {paths.slice(26, 39).map((filename, index) => (
           <img
-            class='card-loader__img'
+            class="card-loader__img"
             key={filename}
             onAbort={onError}
             onError={onError}
@@ -61,13 +65,13 @@ export function CardLoader ({
           />
         ))}
       </div>
-      <div class='card-loader__suit'>
-        {filenames.slice(39).map((filename, index) => (
+      <div class="card-loader__suit">
+        {paths.slice(39).map((filename, index) => (
           <img
-            class='card-loader__img'
+            class="card-loader__img"
             key={filename}
             onAbort={onError}
-            onError={onError}
+            onError={console.log}
             onLoad={handleComplete}
             src={filename}
             alt={imgAlts[index]}
